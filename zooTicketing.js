caf36b1312ec977398329686764d1dfab7010936
tejal-supe <supetejalc@gmail.com>
2024-02-06 09:51:32 +0530

    functional approach for the problem statement

--- zooTicketing.js
+++ zooTicketing.js
@@ -22,8 +22,41 @@
 // Security
 // get ticket data ==> Guest 1 (age : 15) Guest 2 (age : 20)
 
-class Guest {
-  constructor(num, age) {
-    this.guests = [{ num: age }];
-  }
+// function approach 
+function gatePass(age) {
+    const entrancePrice = getTheRateOnAge(age);
+    return { age, entrancePrice };
 }
+function getTheRateOnAge(age){
+    if(age<=2){
+        return 0;
+    }else if(age > 2 && age < 18){
+        return 100;
+    }else if(age >=18 && age<60){
+        return 500;
+    }
+    else{
+        return 300;
+    }
+}
+function totalGuests(data,price){
+    const total = data.length;
+    console.log(`Your entrance ticket is issued for ${total} guests and youe total charges are ${price}`)
+
+}
+
+function securityCheck(data){
+    data.map((d,id)=>{
+        console.log(`Guest ${id + 1} (age : ${d.age})`)
+    } )
+}
+
+function issueTicket(age){
+        const ticket = age.map(a=>gatePass(a))
+        const totalPrice = ticket.reduce((acc,cuur)=> getTheRateOnAge(cuur) + acc ,0)
+        totalGuests(ticket,totalPrice)
+        securityCheck(ticket)
+        return totalPrice;
+}
+
+const pass = issueTicket([15,20])
